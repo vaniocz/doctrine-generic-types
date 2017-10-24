b23de16dffe6e49357fd38c62ddb17489a62f8c4
maryo <mario.dweller@seznam.cz>
2017-10-24 21:35:18 +0200

    Added support for automatically populating discriminator field when it is part of ID

---
+++ src/Patches/ORM/Id/AssignedGenerator.php
@@ -0,0 +1,39 @@
+<?php
+namespace Doctrine\ORM\Id;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
+
+/**
+ * Automatically populates discriminator field when it is part of ID.
+ */
+class AssignedGenerator extends AbstractIdGenerator
+{
+    /**
+     * @param EntityManager $entityManager
+     * @param mixed $entity
+     * @throws ORMException
+     * @return array
+     */
+    public function generate(EntityManager $entityManager, $entity): array
+    {
+        $classMetadata = $entityManager->getClassMetadata(get_class($entity));
+        $id = [];
+
+        foreach ($classMetadata->getIdentifierFieldNames() as $field) {
+            $value = $classMetadata->discriminatorColumn && $field === $classMetadata->discriminatorColumn['fieldName']
+                ? $classMetadata->discriminatorValue
+                : $classMetadata->getFieldValue($entity, $field);
+
+            if ($value === null) {
+                throw ORMException::entityMissingAssignedIdForField($entity, $field);
+            } elseif (isset($classMetadata->associationMappings[$field])) {
+                $value = $entityManager->getUnitOfWork()->getSingleIdentifierValue($value);
+            }
+
+            $id[$field] = $value;
+        }
+
+        return $id;
+    }
+}
